b006ba5f219476af9ca5722db54e1a3a97b6e688
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2025-02-03 16:25:14 +0000

    added some styling

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,7 +2,7 @@ import Citation from "../Citation";
 
 export default function Card() {
   return (
-    <div>
+    <div className="card">
       <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
 
       <Citation />
