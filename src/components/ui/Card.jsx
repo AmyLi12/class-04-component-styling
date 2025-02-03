9ad71e2cedb503d53c24705d7c632b7dd11e502f
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2025-02-03 16:17:03 +0000

    fixed link for static resource

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -3,7 +3,7 @@ import Citation from "../Citation";
 export default function Card() {
   return (
     <div>
-      <img src="../../images/galaxy.webp" alt="an AI generated galaxy" />
+      <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
 
       <Citation />
     </div>
