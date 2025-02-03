e812d6a10f2e647eca7fde373ffaaeab83386246
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2025-02-03 16:12:25 +0000

    conpleted class activity

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,13 @@
+import Gallery from './components/Gallery'
+
 export default function Home() {
+  return (
   <main>
     <Gallery>
 
       <h2>Rendered Date</h2>
       <p>{new Date().toLocaleString()}</p>
+    </Gallery>
   </main>
+  )
 }
